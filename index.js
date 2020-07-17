42ba67993a2042caf70a0a25f20beb0f036ab7bb
Sckz <57728145+Sckz@users.noreply.github.com>
2020-07-17 18:12:59 +0200

    Update index.js

--- index.js
+++ index.js
@@ -55,11 +55,16 @@ client.on("message", async message => {
 });
 //Join
 client.on('guildMemberAdd', member => {
+let phrase = [
+`**Bienvenue à __**${member.user}**__ qui a rejoint le discord 💎.**`,
+`**Oh! Un __**${member.user}**__ est apparu ! Vite il faut le capturer !**`,
+`**OMG! Il y a le fameux __**${member.user}**__ qui est la ! ✨`
+]
 	let embed = new Discord.RichEmbed()
 	 .setColor(16250871)
 	 .setTitle("🛬 ▌__**NOUVEL ARRIVANT !**__")
 	 .setThumbnail(member.user.displayAvatarURL)
-	 .setDescription(`**Bienvenue à **${member.user}** qui a rejoint le discord 💎.**\n__**Amuse-toi bien ! 💘**__`)
+	 .setDescription(`**Bienvenue à __**${member.user}**__ qui a rejoint le discord 💎.**\n__**Amuse-toi bien ! 💘**__`)
 	 .setImage("https://media.giphy.com/media/9CihaEMp5sORi/giphy.gif")
          .setFooter(`Hakitsuke 🥀 | Utilisateurs : ${member.guild.memberCount}`)
 
