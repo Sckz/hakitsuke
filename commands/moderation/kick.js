e02ad5aaf3639c333d35e68f36c179851f534dcb
Sckz <57728145+Sckz@users.noreply.github.com>
2020-08-02 17:30:43 +0200

    Update kick.js

--- commands/moderation/kick.js
+++ commands/moderation/kick.js
@@ -7,14 +7,29 @@ module.exports = {
     usage: "<utilisateur> [raison]",
     run: async (client, message, args) => {
         message.channel.bulkDelete(parseInt(1))
-        if (!message.member.hasPermission('KICK_MEMBERS')) return message.channel.send("> Vous n'avez pas la permission !").then(m => m.delete(5000));
+        let e1 = new Discord.RichEmbed()
+         .setAuthor("Une erreur est survenue", 'https://emoji.gg/assets/emoji/7685_no.png')
+         .setDescription(`**Vous n'avez pas la permission !**`)
+         .setFooter('Ce message se supprime automatiquement.')
+        if (!message.member.hasPermission('KICK_MEMBERS')) return message.channel.send(e1).then(m => m.delete(5000));
         let member = message.mentions.members.first()
         let kickReason = args.slice(1).join(' ')
-        if (!kickReason) return kickReason = "Aucune"
-        if (!member) return message.channel.send("> Veuillez mentionner un utilisateur !").then(m => m.delete(5000));
-        if (!kickReason) return kickReason = "Aucune raison définit"
-        if (member.highestRole.calculatedPosition >= message.member.highestRole.calculatedPosition && message.author.id !== message.guild.owner.id) return message.channel.send("> Vous ne pouvez pas ban cet utilisateur !").then(m => m.delete(5000));
-        if (!member.kickable) return message.channel.send("> Je ne peux pas kick cet utilisateur").then(m => m.delete(5000));
+        let e2 = new Discord.RichEmbed()
+         .setAuthor("Une erreur est survenue", 'https://emoji.gg/assets/emoji/7685_no.png')
+         .setDescription(`**Veuillez définir une raison.**`)
+         .setFooter('Ce message se supprime automatiquement.')
+        if (!kickReason) return message.channel.send(e2)
+        let e3 = new Discord.RichEmbed()
+         .setAuthor("Une erreur est survenue", 'https://emoji.gg/assets/emoji/7685_no.png')
+         .setDescription(`**Veuillez mentionner un utilisateur.**`)
+         .setFooter('Ce message se supprime automatiquement.')
+        if (!member) return message.channel.send(e3).then(m => m.delete(5000));
+        let e4 = new Discord.RichEmbed()
+         .setAuthor("Une erreur est survenue", 'https://emoji.gg/assets/emoji/7685_no.png')
+         .setDescription(`**Il est impossible de sanctionner cette utilisateur**`)
+         .setFooter('Ce message se supprime automatiquement.')
+        if (member.highestRole.calculatedPosition >= message.member.highestRole.calculatedPosition && message.author.id !== message.guild.owner.id) return message.channel.send(e4).then(m => m.delete(5000));
+        if (!member.kickable) return message.channel.send(e4).then(m => m.delete(5000));
         member.kick()
          let send = new Discord.RichEmbed()
         .setAuthor("Sanction appliquée", 'https://emoji.gg/assets/emoji/2990_yes.png')
