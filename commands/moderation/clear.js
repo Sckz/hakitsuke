e2b60cc63dc9a1cb958bf9e3cd14f7c52d5590c9
Sckz <57728145+Sckz@users.noreply.github.com>
2020-08-02 17:06:45 +0200

    Update clear.js

--- commands/moderation/clear.js
+++ commands/moderation/clear.js
@@ -6,16 +6,32 @@ module.exports = {
     description: "Supprimer des messages.",
     usage: "<nombre de message(s)>",
     run: async (client, message, args) => {
-        if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send("> Vous n'avez pas la permission !").then(m => m.delete(5000));
+        let erreur1 = new Discord.RichEmbed()
+         .setAuthor("Une erreur est survenue", 'https://emoji.gg/assets/emoji/7685_no.png')
+         .setDescription(`**Vous n'avez la permission !**`)
+         .setFooter('Ce message se supprime automatiquement.')
+        if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send(erreur1).then(m => m.delete(5000));
         let count = args[0]
-        if (!count) return message.channel.send("> Veuillez indiquer un nombre de messages à supprimer !").then(m => m.delete(5000));
-        if (isNaN(count)) return message.channel.send("> Veuillez indiquer un nombre valide").then(m => m.delete(5000));
-        if (count < 1 || count > 100) return message.channel.send("> Veuillez indiquer un nombre entre 1 et 80").then(m => m.delete(5000));
+        let erreur2 = new Discord.RichEmbed()
+         .setAuthor("Une erreur est survenue", 'https://emoji.gg/assets/emoji/7685_no.png')
+         .setDescription(`**Veuillez indiquer un nombre de message à supprimer !**`)
+         .setFooter('Ce message se supprime automatiquement.')
+        if (!count) return message.channel.send(erreur2).then(m => m.delete(5000));
+        let erreur3 = new Discord.RichEmbed()
+         .setAuthor("Une erreur est survenue", 'https://emoji.gg/assets/emoji/7685_no.png')
+         .setDescription(`**Veuillez indiquer un nombre valide !**`)
+         .setFooter('Ce message se supprime automatiquement.')
+        if (isNaN(count)) return message.channel.send(erreur3).then(m => m.delete(5000));
+        let erreur4 = new Discord.RichEmbed()
+         .setAuthor("Une erreur est survenue", 'https://emoji.gg/assets/emoji/7685_no.png')
+         .setDescription(`**Veuillez indiquer un nombre entre 1 et 80 !**`)
+         .setFooter('Ce message se supprime automatiquement.')
+        if (count < 1 || count > 100) return message.channel.send(erreur4).then(m => m.delete(5000));
         let send = new Discord.RichEmbed()
          .setAuthor("Commande effectuée", 'https://emoji.gg/assets/emoji/2990_yes.png')
          .setDescription("``" + count + "`` **messages ont été supprimés**")
          .setFooter('Ce message se supprime automatiquement.')
         message.channel.bulkDelete(parseInt(count) + 1)
-        message.channel.send(send).then(m => m.delete(15000));
+        message.channel.send(send).then(m => m.delete(10000));
     }
 }
